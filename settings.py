e7f3020b3dfaa7c24736c5b580fb91e4dac7f923
Anatoly <solartune@gmail.com>
2021-11-25 16:16:24 +0100

    Init the project

---
+++ settings.py
@@ -0,0 +1,56 @@
+################################
+# Commented lines are optional #
+################################
+
+
+TENANT_ID_GETTER = NotImplemented
+
+EXTERNAL_APP_NAME = NotImplemented
+
+
+# ALLOWED_ORIGINS: List[str]
+
+
+MUTABLE_SETTINGS = NotImplemented
+
+GET_TEST_MUTABLE_SETTINGS = NotImplemented
+
+
+# STRUCTLOG_CONTEXTVARS: Callable[['TenantSettings'], Dict[str, Any]]
+
+
+UI_HTML_PATH = NotImplemented
+
+
+PERMISSIONS = NotImplemented
+
+WEBHOOK_EVENTS = NotImplemented
+
+WEBHOOK_DATA_TYPE = NotImplemented
+
+WEBHOOK_SEND_DATA: NotImplemented
+
+
+# signals
+
+
+# REGISTER_PRE_SIGNALS: List[Signal]
+# REGISTER_POST_SIGNALS: List[Signal]
+
+# CONFIG_GET_PRE_SIGNALS: List[Signal]
+# CONFIG_GET_POST_SIGNALS: List[Signal]
+
+# CONFIG_PUT_PRE_SIGNALS: List[Signal]
+# CONFIG_PUT_POST_SIGNALS: List[Signal]
+
+# CONFIG_PATCH_PRE_SIGNALS: List[Signal]
+# CONFIG_PATCH_POST_SIGNALS: List[Signal]
+
+# LOGS_PRE_SIGNALS: List[Signal]
+# LOGS_POST_SIGNALS: List[Signal]
+
+# STARTUP_SIGNALS: List[Signal]
+# SHUTDOWN_SIGNALS: List[Signal]
+
+# WEBHOOK_PRE_SIGNALS: List[Signal]
+# WEBHOOK_POST_SIGNALS: List[Signal]
