192418cd5191654f676226f93fd69fbb30de876e
Michal Zajac <michal.99.zajac@gmail.com>
2021-12-20 11:14:14 +0100

    feat(layout): add layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { TenantProvider } from 'saleor-app-ui'
 import { ThemeProvider } from '@saleor/macaw-ui'
 
+import Layout from './components/Layout'
 import './App.css'
 
 function App() {
   return (
     <ThemeProvider>
       <TenantProvider>
-        <h1>Hello World</h1>
+        <Layout
+          header={<div>Header</div>}
+          content={<div>Content</div>}
+          footer={<div>Footer</div>}
+        />
       </TenantProvider>
     </ThemeProvider>
   )
