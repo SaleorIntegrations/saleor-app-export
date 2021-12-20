853c449bcaf45db39b6451e5c871b4bff5cbc73a
Michal Zajac <michal.99.zajac@gmail.com>
2021-12-20 10:32:07 +0100

    feat(saleor): add saleor-app-ui dep

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { TenantProvider } from 'saleor-app-ui'
+import { ThemeProvider } from '@saleor/macaw-ui'
+
 import './App.css'
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <ThemeProvider>
+      <TenantProvider>
+        <h1>Hello World</h1>
+      </TenantProvider>
+    </ThemeProvider>
+  )
 }
 
 export default App
