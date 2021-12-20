c6e3ae0976725359318d700c1a4b3a54fa519914
Michal Zajac <michal.99.zajac@gmail.com>
2021-12-20 12:38:39 +0100

    feat(header): add header component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,18 +3,19 @@ import { TenantProvider } from 'saleor-app-ui'
 import { ThemeProvider } from '@saleor/macaw-ui'
 
 import Layout from './components/Layout'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <ThemeProvider>
-      <TenantProvider>
-        <Layout
-          header={<div>Header</div>}
-          content={<div>Content</div>}
-          footer={<div>Footer</div>}
-        />
-      </TenantProvider>
+      {/* <TenantProvider> */}
+      <Layout
+        header={<Header />}
+        content={<div>Content</div>}
+        footer={<div>Footer</div>}
+      />
+      {/* </TenantProvider> */}
     </ThemeProvider>
   )
 }
