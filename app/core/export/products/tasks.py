1e80930c10ef4c34536a9ad98950c48963c8b25f
Krzysztof Kulig <kris@tango.agency>
2021-12-28 11:37:11 +0100

    Remove unused code

--- app/core/export/products/tasks.py
+++ app/core/export/products/tasks.py
@@ -1,21 +1,14 @@
-from typing import Dict, Union, TYPE_CHECKING
-import asyncio
+from typing import TYPE_CHECKING, Dict, Union
 
-from db import get_db
 from app.core.reports.models import ExportFile
-from app.core.common.tasks import on_task_failure, on_task_success
-from app.celery import app
+from db import get_db
+
 from .utils.export import export_products
 
 if TYPE_CHECKING:
     from app.graphql.reports.mutations.products import ExportInfoInput
 
 
-# TODO Do we need celery here at all?
-# Alternatively, we could make it a background task using fastapi.
-# In this case we don't need to worry about celery not supporting asyncio.
-
-# @app.task(on_success=on_task_success, on_failure=on_task_failure)
 async def export_products_task(
     report_id: int,
     scope: Dict[str, Union[str, dict]],
@@ -29,6 +22,3 @@ async def export_products_task(
     db.add(export_file)
     await db.commit()
     await export_products(export_file, scope, export_info, file_type, delimiter)
-    # loop = asyncio.get_event_loop()
-    # coroutine = export_products(export_file, scope, export_info, file_type, delimiter)
-    # loop.run_until_complete(coroutine)
