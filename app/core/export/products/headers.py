80176a8905fd391181bac697f39a2d278cbb396a
Krzysztof Kulig <kris@tango.agency>
2021-12-29 15:32:39 +0100

    Add tests for header generation

--- app/core/export/products/headers.py
+++ app/core/export/products/headers.py
@@ -22,6 +22,16 @@ FIELD_ENUM_TO_HEADER_MAP = {
 def get_headers(column_info: ProductSelectedColumnsInfo) -> List[str]:
     """Get headers for both static and dynamic fields"""
     headers = get_static_headers(column_info.fields)
+
+    for attribute in column_info.attributes:
+        headers.extend(get_attribute_headers(attribute))
+
+    for channel in column_info.channels:
+        headers.extend(get_channel_headers(channel))
+
+    for warehouse in column_info.warehouses:
+        headers.extend(get_warehouse_headers(warehouse))
+
     return headers
 
 
@@ -29,4 +39,24 @@ def get_static_headers(fields: List[ProductFieldEnum]):
     """Return static headers according to their order in the enum"""
     return [
         FIELD_ENUM_TO_HEADER_MAP[field] for field in ProductFieldEnum if field in fields
+    ]
+
+
+def get_attribute_headers(attribute: str):
+    """Return headers for a single attribute"""
+    return [attribute]
+
+
+def get_channel_headers(channel: str):
+    """Return headers for a single channel"""
+    return [
+        f"Price ({channel})",
+        f"Currency ({channel})",
+    ]
+
+
+def get_warehouse_headers(warehouse: str):
+    """Return headers for a single warehouse"""
+    return [
+        f"Stock quantity ({warehouse})",
     ]
---
+++ app/core/export/products/tests/test_headers.py
@@ -0,0 +1,89 @@
+from unittest import mock
+
+import pytest
+
+from app.core.export.products.fields import ProductFieldEnum
+from app.core.export.products.headers import (
+    FIELD_ENUM_TO_HEADER_MAP,
+    get_attribute_headers,
+    get_channel_headers,
+    get_headers,
+    get_static_headers,
+    get_warehouse_headers,
+)
+
+
+@pytest.mark.asyncio
+async def test_get_static_headers():
+    # given
+    reversed_fields = list(reversed(ProductFieldEnum))
+    # when
+    headers = get_static_headers(reversed_fields)
+    # then
+    assert len(headers) == len(ProductFieldEnum)
+    for i, field in enumerate(ProductFieldEnum):
+        assert headers[i] == FIELD_ENUM_TO_HEADER_MAP[field]
+
+
+@pytest.mark.asyncio
+async def test_get_attribute_headers():
+    # given
+    attribute_id = "Attribute:1"
+    # when
+    headers = get_attribute_headers(attribute_id)
+    # then
+    assert len(headers) == 1
+    assert headers[0] == attribute_id
+
+
+@pytest.mark.asyncio
+async def test_get_channel_headers():
+    # given
+    channel_id = "Channel:1"
+    # when
+    headers = get_channel_headers(channel_id)
+    # then
+    assert len(headers) == 2
+    assert headers[0] == f"Price ({channel_id})"
+    assert headers[1] == f"Currency ({channel_id})"
+
+
+@pytest.mark.asyncio
+async def test_get_warehouse_headers():
+    # given
+    warehouse_id = "Warehouse:1"
+    # when
+    headers = get_warehouse_headers(warehouse_id)
+    # then
+    assert len(headers) == 1
+    assert headers[0] == f"Stock quantity ({warehouse_id})"
+
+
+@pytest.mark.asyncio
+@mock.patch("app.core.export.products.headers.get_warehouse_headers")
+@mock.patch("app.core.export.products.headers.get_channel_headers")
+@mock.patch("app.core.export.products.headers.get_attribute_headers")
+@mock.patch("app.core.export.products.headers.get_static_headers")
+async def test_get_headers(
+    m_static, m_attribute, m_channel, m_warehouse, product_column_info
+):
+    # given
+    product_column_info.warehouses = ["1", "2"]
+    product_column_info.channels = ["3", "4"]
+    product_column_info.attributes = ["5", "6"]
+    a = ["a"]
+    m_static.return_value = a
+    b = ["b"]
+    c = ["c"]
+    m_attribute.side_effect = [b, c]
+    d = ["d"]
+    e = ["e"]
+    m_channel.side_effect = [d, e]
+    f = ["f"]
+    g = ["g"]
+    m_warehouse.side_effect = [f, g]
+    # when
+    headers = get_headers(product_column_info)
+    # then
+    assert len(headers) == 7
+    assert headers == ["a", "b", "c", "d", "e", "f", "g"]
