2448eaa896429eff03570d1a52c3b04f90351463
Krzysztof Kulig <kris@tango.agency>
2022-01-16 21:11:07 +0100

    Add unit tests for fetching recipients with exceptions flag set

---
+++ app/core/export/tests/test_fetch.py
@@ -0,0 +1,48 @@
+from unittest import mock
+
+import pytest
+
+from app.core.export.fetch import fetch_recipients
+from app.core.export.fields import RecipientInfo
+
+
+@pytest.mark.asyncio
+@mock.patch("app.core.export.fetch.parse_recipients_response")
+@mock.patch("app.core.export.fetch.execute_query")
+async def test_fetch_recipients_raises_exception_for_invalid_user(
+    m_execute,
+    m_parse,
+):
+    # given
+    m_parse.return_value = {"User:1": ["test@example.com"]}, {}
+    recipient_info = RecipientInfo(users=["User:1", "User:2"])
+
+    # when
+    with pytest.raises(ValueError) as e:
+        await fetch_recipients(recipient_info, raise_exception=True)
+
+    # then
+    assert m_execute.call_count == 1
+    assert m_parse.call_count == 1
+    assert "User:2" in str(e)
+
+
+@pytest.mark.asyncio
+@mock.patch("app.core.export.fetch.parse_recipients_response")
+@mock.patch("app.core.export.fetch.execute_query")
+async def test_fetch_recipients_raises_exception_for_invalid_group(
+    m_execute,
+    m_parse,
+):
+    # given
+    m_parse.return_value = {}, {"Group:1": ["test@example.com"]}
+    recipient_info = RecipientInfo(permission_groups=["Group:1", "Group:2"])
+
+    # when
+    with pytest.raises(ValueError) as e:
+        await fetch_recipients(recipient_info, raise_exception=True)
+
+    # then
+    assert m_execute.call_count == 1
+    assert m_parse.call_count == 1
+    assert "Group:2" in str(e)
