43c9e8bcc30e6193a6bce0d3ddf232149f45bd64
Krzysztof Kulig <kris@tango.agency>
2022-01-16 21:14:52 +0100

    Test fetching recipients while ignoring errors

--- app/core/export/tests/test_fetch.py
+++ app/core/export/tests/test_fetch.py
@@ -9,6 +9,34 @@ from app.core.export.fields import RecipientInfo
 @pytest.mark.asyncio
 @mock.patch("app.core.export.fetch.parse_recipients_response")
 @mock.patch("app.core.export.fetch.execute_query")
+async def test_fetch_recipients_returns_list(
+    m_execute,
+    m_parse,
+):
+    # given
+    mail_1 = "user1@example.com"
+    mail_2 = "group2@example.com"
+    m_parse.return_value = (
+        {"User:1": [mail_1]},
+        {"Group:2": [mail_2]},
+    )
+    recipient_info = RecipientInfo(
+        users=["User:1", "User:2"],
+        permission_groups=["Group:1", "Group:2"],
+    )
+
+    # when
+    emails = await fetch_recipients(recipient_info)
+
+    # then
+    assert m_execute.call_count == 1
+    assert m_parse.call_count == 1
+    assert emails == [mail_1, mail_2]
+
+
+@pytest.mark.asyncio
+@mock.patch("app.core.export.fetch.parse_recipients_response")
+@mock.patch("app.core.export.fetch.execute_query")
 async def test_fetch_recipients_raises_exception_for_invalid_user(
     m_execute,
     m_parse,
