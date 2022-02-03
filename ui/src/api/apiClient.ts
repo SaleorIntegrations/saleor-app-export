a6d1e88c52edba7b3edfa740f565c57065a25c11
Michal Zajac <michal.99.zajac@gmail.com>
2022-02-03 14:54:46 +0100

    fix(api): fix app url

--- ui/src/api/apiClient.ts
+++ ui/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'urql'
 
 export const apiClient = createClient({
-  url: `${process.env.REACT_APP_APP_URL}/graphql/`,
+  url: `${process.env.REACT_APP_APP_URL}/graphql`,
 })
 
 export default apiClient
